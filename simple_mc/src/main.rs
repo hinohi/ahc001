89dadb57c19bee036e752ecde7a1c4b088e53a5a
Nakayama Daiju <42.daiju@gmail.com>
2021-03-07 14:26:38 +0900

    qtree

--- simple_mc/src/main.rs
+++ simple_mc/src/main.rs
@@ -8,8 +8,170 @@ use rand::{
 use rand_pcg::Mcg128Xsl64;
 
 const L: i16 = 10_000;
+const Q_MIN: i16 = L / 2 / 2 / 2;
+const LAYER1_OFFSET: u8 = (4 - 1) / 3;
+const LAYER2_OFFSET: u8 = (4 * 4 - 1) / 3;
+const LAYER3_OFFSET: u8 = (4 * 4 * 4 - 1) / 3;
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+/// 最大4bitの情報がある n を受け取る
+/// 00 00 ab cd
+/// ↓
+/// 00 ab ** cd
+/// ↓
+/// 00 ab 00 cd
+/// ↓
+/// 0a *b 0c *d
+/// ↓
+/// 0a 0b 0c 0d
+fn bit_separate(mut n: u8) -> u8 {
+    n = (n | (n << 2)) & 0b_0011_0011;
+    n = (n | (n << 1)) & 0b_0101_0101;
+    n
+}
+
+fn point_to_leaf_gid(x: i16, y: i16) -> u8 {
+    let x = (x / Q_MIN) as u8;
+    let y = (y / Q_MIN) as u8;
+    bit_separate(x) | (bit_separate(y) << 1)
+}
+
+fn leaf_leaf_to_gid(i: u8, j: u8) -> u8 {
+    let mut n = i ^ j;
+    // layer 3
+    if n == 0 {
+        return LAYER3_OFFSET + j;
+    }
+    // layer 2
+    n >>= 2;
+    if n == 0 {
+        return LAYER2_OFFSET + (j >> 2);
+    }
+    // layer 1
+    n >>= 2;
+    if n == 0 {
+        return LAYER1_OFFSET + (j >> 4);
+    }
+    // layer 0 = root
+    0
+}
+
+pub fn get_gid(rect: &Rect) -> u8 {
+    leaf_leaf_to_gid(
+        point_to_leaf_gid(rect.x1, rect.y1),
+        point_to_leaf_gid(rect.x2, rect.y2),
+    )
+}
+
+pub fn parent_gid(gid: u8) -> u8 {
+    if gid >= LAYER3_OFFSET {
+        return LAYER2_OFFSET + (gid - LAYER3_OFFSET) / 4;
+    }
+    if gid >= LAYER2_OFFSET {
+        return LAYER1_OFFSET + (gid - LAYER2_OFFSET) / 4;
+    }
+    0
+}
+
+pub fn children_gid_range(gid: u8) -> std::ops::Range<u8> {
+    // gid == 0 と gid >= LAYER3_OFFSET はこない
+    if gid < LAYER2_OFFSET {
+        let start = (gid - LAYER1_OFFSET) * 4 + LAYER2_OFFSET;
+        let end = (gid - LAYER1_OFFSET + 1) * 4 + LAYER2_OFFSET;
+        return start..end;
+    }
+    let start = (gid - LAYER2_OFFSET) * 4 + LAYER3_OFFSET;
+    let end = (gid - LAYER2_OFFSET + 1) * 4 + LAYER3_OFFSET;
+    return start..end;
+}
+
+#[derive(Debug)]
+pub struct QTree {
+    grid: Vec<Vec<usize>>,
+}
+
+impl QTree {
+    pub fn new(rects: &[Rect]) -> QTree {
+        const N: usize = 1 + 4 + 4 * 4 + 4 * 4 * 4;
+        let mut grid = Vec::with_capacity(N);
+        for _ in 0..N {
+            grid.push(Vec::new());
+        }
+        for (i, rect) in rects.iter().enumerate() {
+            let gid = get_gid(rect) as usize;
+            grid[gid].push(i);
+        }
+        QTree { grid }
+    }
+
+    fn intersect_one_grid(&self, gid: u8, new: &Rect, i: usize, rects: &[Rect]) -> bool {
+        for &j in self.grid[gid as usize].iter() {
+            if i == j {
+                continue;
+            }
+            if new.intersect(&rects[j]) {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn intersect_to_parent(&self, mut gid: u8, new: &Rect, i: usize, rects: &[Rect]) -> bool {
+        loop {
+            if self.intersect_one_grid(gid, new, i, rects) {
+                return true;
+            }
+            if gid == 0 {
+                break false;
+            }
+            gid = parent_gid(gid);
+        }
+    }
+
+    pub fn intersect_to_children(&self, gid: u8, new: &Rect, i: usize, rects: &[Rect]) -> bool {
+        if gid >= LAYER3_OFFSET {
+            return false;
+        }
+        let mut stack = vec![children_gid_range(gid)];
+        while let Some(children) = stack.pop() {
+            for c in children {
+                if self.intersect_one_grid(c, new, i, rects) {
+                    return true;
+                }
+                if c < LAYER3_OFFSET {
+                    stack.push(children_gid_range(c));
+                }
+            }
+        }
+        false
+    }
+
+    pub fn intersect(&self, new: &Rect, i: usize, rects: &[Rect]) -> bool {
+        let gid = get_gid(new);
+        if gid == 0 {
+            return intersect(new, i, rects);
+        }
+        if self.intersect_to_parent(gid, new, i, rects) {
+            return true;
+        }
+        if self.intersect_to_children(gid, new, i, rects) {
+            return true;
+        }
+        false
+    }
+
+    pub fn update(&mut self, new: &Rect, old: &Rect, i: usize) {
+        let old_gid = get_gid(old) as usize;
+        let new_gid = get_gid(new) as usize;
+        if old_gid == new_gid {
+            return;
+        }
+        let pos = self.grid[old_gid].iter().position(|j| *j == i).unwrap();
+        self.grid[old_gid].swap_remove(pos);
+        self.grid[new_gid].push(i);
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct Rect {
     pub x1: i16,
     pub x2: i16,
@@ -153,6 +315,7 @@ fn mc(
     let grow_d = Uniform::new(1, 8 + 1);
     let prob_d = Uniform::new(0.0, 1.0);
 
+    let mut qtree = QTree::new(rects);
     let mut rects = rects.to_vec();
     let mut best = rects.clone();
     let mut best_score = score;
@@ -205,12 +368,16 @@ fn mc(
                 let score_diff = new_score - scores[i];
                 if score_diff >= 0.0 || prob_d.sample(rng) < (score_diff * beta).exp() {
                     let intersected = match id {
-                        IntersectDirection::X | IntersectDirection::Y => intersect(&new, i, &rects),
+                        IntersectDirection::X | IntersectDirection::Y => {
+                            qtree.intersect(&new, i, &rects)
+                        }
                         IntersectDirection::None => false,
                     };
                     if intersected {
                         continue;
                     }
+
+                    qtree.update(&new, &rects[i], i);
                     scores[i] = new_score;
                     rects[i] = new;
                     score += score_diff;
@@ -256,5 +423,25 @@ fn main() {
     let (_, best) = mc(&mut rng, &rects, &target, &size);
     for rect in best {
         println!("{} {} {} {}", rect.x1, rect.y1, rect.x2, rect.y2);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bit_separate() {
+        assert_eq!(bit_separate(0b000000), 0b000000);
+        assert_eq!(bit_separate(0b000001), 0b000001);
+        assert_eq!(bit_separate(0b000111), 0b010101);
+        assert_eq!(bit_separate(0b000100), 0b010000);
+        assert_eq!(bit_separate(0b000101), 0b010001);
+    }
+
+    #[test]
+    fn test_point_to_leaf_gid() {
+        assert_eq!(point_to_leaf_gid(0, 0), 0);
+        assert_eq!(point_to_leaf_gid(L - 1, L - 1), 4 * 4 * 4 - 1);
     }
 }
