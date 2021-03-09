9d3ac1ada69975eb80cfe784d9b7fa29df896817
Nakayama Daiju <42.daiju@gmail.com>
2021-03-09 21:13:25 +0900

    Lambdaで計算するバージョンの optuna

--- opt.py
+++ opt.py
@@ -1,94 +1,94 @@
-import logging
-import sys
 import json
-import subprocess
+import time
+import uuid
+import sys
 
+import boto3
 import optuna
 
+N = int(sys.argv[1])
 
-optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
-logger = optuna.logging.get_logger('run')
-study_name = "simple-mc_non-n-specified_100_2"
-storage_name = "sqlite:///{}.db".format(study_name)
+QUEUE_URL = 'https://sqs.ap-northeast-1.amazonaws.com/169698630369/ahc001-Queue4A7E3555-RODR3OR8LUGQ'
+
+storage_name = 'sqlite:///sa.db'
 study = optuna.create_study(
-    study_name=study_name,
+    study_name=f'sa-{N}',
     storage=storage_name,
     load_if_exists=True,
 )
 
+lambda_client = boto3.client('lambda')
+sqs_client = boto3.client('sqs')
 
-def run(param: str) -> float:
-    results = []
-    worker_num = 4
-    workers = []
 
-    def wait():
-        tmp_workers = []
-        for p in workers:
-            try:
-                code = p.wait(0.1)
-            except subprocess.TimeoutExpired:
-                tmp_workers.append(p)
-                continue
-            if code == 0:
-                results.append(n)
-            else:
-                logger.warning(p.stderr.read().decode())
-        workers[:] = tmp_workers
-
-    for n in range(100):
-        in_file = f'tools/in/{n:04}.txt'
-        out_file = f'out/{n:04}.txt'
-        p = subprocess.Popen(
-            args=f"./target/release/simple_mc '{param}' < {in_file} > {out_file}",
-            shell=True,
-            stderr=subprocess.PIPE,
+def invoke(arg: str) -> set[str]:
+    message_ids = set()
+    for seed in range(100):
+        message_id = str(uuid.uuid4())
+        lambda_client.invoke(
+            FunctionName='ahc001',
+            InvocationType='Event',
+            LogType='None',
+            Payload=json.dumps({
+                'message_id': message_id,
+                'n': N,
+                'seed': seed,
+                'arg': arg,
+            }),
         )
-        workers.append(p)
-        while len(workers) == worker_num:
-            wait()
-    while workers:
-        wait()
+        message_ids.add(message_id)
+    return message_ids
 
-    score = 0
-    for n in results:
-        p = subprocess.run(
-            ['./target/release/vis', f'tools/in/{n:04}.txt', f'out/{n:04}.txt'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            timeout=10.0,
+
+def receive(message_ids: set[str]) -> list[float]:
+    scores = []
+    receipt_handles = {}
+    while message_ids:
+        r = sqs_client.receive_message(
+            QueueUrl=QUEUE_URL,
+            MaxNumberOfMessages=10,
         )
-        if p.stderr:
-            logger.warning(p.stderr.decode())
-        try:
-            score += int(p.stdout.decode().strip())
-        except ValueError:
-            logger.warning(p.stdout)
-    return 1.0 - score / len(results) / 10 ** 9
+        for record in r.get('Messages', []):
+            rh = record['ReceiptHandle']
+            receipt_handles[str(uuid.uuid4())] = rh
+            body = json.loads(record['Body'])['responsePayload']
+            message_ids.remove(body['message_id'])
+            scores.append(body['score'])
+        if receipt_handles:
+            deleted = sqs_client.delete_message_batch(
+                QueueUrl=QUEUE_URL,
+                Entries=[{'Id': key, 'ReceiptHandle': rh} for key, rh in receipt_handles.items()],
+            )
+            for suc in deleted.get('Successful', []):
+                receipt_handles.pop(suc['Id'], None)
+        elif message_ids:
+            time.sleep(5)
+    return scores
+
+
+def run(arg: str) -> float:
+    message_ids = invoke(arg)
+    time.sleep(5)
+    scores = receive(message_ids)
+    return 1.0 - sum(scores) / len(scores)
 
 
 def objective(trial: optuna.Trial) -> float:
-    temp0 = trial.suggest_loguniform('temp0', 0.1, 10.0)
-    temp1 = trial.suggest_loguniform('temp1', 0.0001, 0.1)
-    move_d_max = trial.suggest_int('move_d_max', 1, 128)
+    temp0 = trial.suggest_loguniform('temp0', 0.01, 1.0)
+    temp1 = trial.suggest_loguniform('temp1', 1e-8, 0.01)
     grow_d1_start = trial.suggest_loguniform('grow_d1_start', 2.0, 4096.0)
     grow_d1_end = trial.suggest_loguniform('grow_d1_end', 2.0, 4096.0)
     grow_d2_start = trial.suggest_loguniform('grow_d2_start', 2.0, 4096.0)
     grow_d2_end = trial.suggest_loguniform('grow_d2_end', 2.0, 4096.0)
-    rect_move_weight = trial.suggest_uniform('rect_move_weight', 0.0, 1.0)
     rect_grow_d1_weight = trial.suggest_uniform('rect_grow_d1_weight', 0.0, 1.0)
-    rect_grow_d2_weight = trial.suggest_uniform('rect_grow_d2_weight', 0.0, 1.0)
     param = json.dumps({
         'temp0': temp0,
         'temp1': temp1,
-        'move_d_max': move_d_max,
         'grow_d1_start': grow_d1_start,
         'grow_d1_end': grow_d1_end,
         'grow_d2_start': grow_d2_start,
         'grow_d2_end': grow_d2_end,
-        'rect_move_weight': rect_move_weight,
         'rect_grow_d1_weight': rect_grow_d1_weight,
-        'rect_grow_d2_weight': rect_grow_d2_weight,
     }, indent=None, separators=(',', ':'))
     return run(param)
 
