bad0ec8ae8a5c337fe2b23373fbb24e69d27b8e7
Nakayama Daiju <42.daiju@gmail.com>
2021-03-12 16:24:11 +0900

    lambdaを使って手動実行する機能と1000サンプルのスコア

---
+++ do_lambda.py
@@ -0,0 +1,74 @@
+import json
+import time
+import uuid
+
+import boto3
+
+QUEUE_URL = 'https://sqs.ap-northeast-1.amazonaws.com/169698630369/ahc001-Queue4A7E3555-X7XRXLQUFTF3'
+
+lambda_client = boto3.client('lambda')
+sqs_client = boto3.client('sqs')
+
+
+def invoke(arg: str = None, *, samples=100) -> dict[str, int]:
+    message_seed = {}
+    for seed in range(samples):
+        message_id = str(uuid.uuid4())
+        lambda_client.invoke(
+            FunctionName='ahc001',
+            InvocationType='Event',
+            LogType='None',
+            Payload=json.dumps({
+                'message_id': message_id,
+                'seed': seed,
+                'arg': arg,
+            }),
+        )
+        message_seed[message_id] = seed
+    return message_seed
+
+
+def receive(message_seed: dict[str, int]) -> dict[int, float]:
+    limit = 300
+    start = time.time()
+    scores = {}
+    receipt_handles = {}
+    while message_seed and (time.time() - start) < limit:
+        r = sqs_client.receive_message(
+            QueueUrl=QUEUE_URL,
+            MaxNumberOfMessages=10,
+        )
+        for record in r.get('Messages', []):
+            rh = record['ReceiptHandle']
+            receipt_handles[str(uuid.uuid4())] = rh
+            body = json.loads(record['Body'])['responsePayload']
+            seed = message_seed.pop(body['message_id'], None)
+            if seed is None:
+                continue
+            scores[seed] = body['score']
+        if receipt_handles:
+            deleted = sqs_client.delete_message_batch(
+                QueueUrl=QUEUE_URL,
+                Entries=[{'Id': key, 'ReceiptHandle': rh} for key, rh in receipt_handles.items()],
+            )
+            for suc in deleted.get('Successful', []):
+                receipt_handles.pop(suc['Id'], None)
+        elif message_seed:
+            time.sleep(3)
+    return scores
+
+
+def sampling(arg: str = None, *, samples=100) -> dict[int, float]:
+    message_seed = invoke(arg, samples=samples)
+    time.sleep(5)
+    return receive(message_seed)
+
+
+def main():
+    scores = sampling(samples=1000)
+    for seed in sorted(scores):
+        print(seed, scores[seed])
+
+
+if __name__ == '__main__':
+    main()
--- opt.py
+++ opt.py
@@ -1,11 +1,7 @@
 import json
-import time
-import uuid
 
-import boto3
 import optuna
-
-QUEUE_URL = 'https://sqs.ap-northeast-1.amazonaws.com/169698630369/ahc001-Queue4A7E3555-RODR3OR8LUGQ'
+from do_lambda import sampling
 
 storage_name = 'sqlite:///sa.db'
 study = optuna.create_study(
@@ -13,63 +9,6 @@ study = optuna.create_study(
     storage=storage_name,
     load_if_exists=True,
 )
-
-lambda_client = boto3.client('lambda')
-sqs_client = boto3.client('sqs')
-
-
-def invoke(arg: str) -> set[str]:
-    message_ids = set()
-    for seed in range(300, 400):
-        message_id = str(uuid.uuid4())
-        lambda_client.invoke(
-            FunctionName='ahc001',
-            InvocationType='Event',
-            LogType='None',
-            Payload=json.dumps({
-                'message_id': message_id,
-                'seed': seed,
-                'arg': arg,
-            }),
-        )
-        message_ids.add(message_id)
-    return message_ids
-
-
-def receive(message_ids: set[str]) -> list[float]:
-    scores = []
-    receipt_handles = {}
-    while message_ids:
-        r = sqs_client.receive_message(
-            QueueUrl=QUEUE_URL,
-            MaxNumberOfMessages=10,
-        )
-        for record in r.get('Messages', []):
-            rh = record['ReceiptHandle']
-            receipt_handles[str(uuid.uuid4())] = rh
-            body = json.loads(record['Body'])['responsePayload']
-            try:
-                message_ids.remove(body['message_id'])
-            except KeyError:
-                continue
-            scores.append(body['score'])
-        if receipt_handles:
-            deleted = sqs_client.delete_message_batch(
-                QueueUrl=QUEUE_URL,
-                Entries=[{'Id': key, 'ReceiptHandle': rh} for key, rh in receipt_handles.items()],
-            )
-            for suc in deleted.get('Successful', []):
-                receipt_handles.pop(suc['Id'], None)
-        elif message_ids:
-            time.sleep(3)
-    return scores
-
-
-def run(arg: str) -> float:
-    message_ids = invoke(arg)
-    time.sleep(5)
-    scores = receive(message_ids)
-    return 1.0 - sum(scores) / len(scores)
 
 
 def objective(trial: optuna.Trial) -> float:
@@ -96,7 +35,8 @@ def objective(trial: optuna.Trial) -> float:
         'rect_grow_d1_weight': rect_grow_d1_weight,
         'rect_slide_weight': rect_slide_weight,
     }, indent=None, separators=(',', ':'))
-    return run(param)
+    scores = sampling(param, samples=100)
+    return 1.0 - sum(scores.values()) / len(scores)
 
 
 study.optimize(objective)
